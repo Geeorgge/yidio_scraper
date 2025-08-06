66d4435e3d0e831958132982d7301c9b81f9a095
Geeorgge <jorge_hernandezvqz@hotmail.com>
2025-08-05 20:44:43 -0600

    fix: format function __str__

--- api/yidio_scraper/models.py
+++ api/yidio_scraper/models.py
@@ -9,9 +9,9 @@ class Movie(models.Model):
     imdb_rating = models.DecimalField(max_digits=3, decimal_places=1, default=0.0)
     description = models.TextField()
 
-class Meta:
-        db_table = 'movies'
-        unique_together = ('title', 'year', 'length')
+    class Meta:
+            db_table = 'movies'
+            unique_together = ('title', 'year', 'length')
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
